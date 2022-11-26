a95cc4f53c6f3ac99940f03b5188c9a511c16534
Henrik Ygge <henrik@meepo.se>
2022-11-26 10:39:42 +0100

    first commit, preparing for aoc

---
+++ src/main/java/Day1.java
@@ -0,0 +1,20 @@
+import util.Util;
+
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        var inputs = Util.readInts();
+        Util.submitPart1(part1(inputs));
+        Util.submitPart2(part2(inputs));
+    }
+
+    private static int part2(List<Integer> inputs) {
+        return 0;
+    }
+
+    private static int part1(List<Integer> inputs) {
+        return 0;
+    }
+}
