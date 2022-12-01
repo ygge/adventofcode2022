0ea29d84bb80963119e5ca68afeaf064435c32df
Henrik Ygge <henrik@meepo.se>
2022-12-01 06:07:54 +0100

    solved day 1

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,20 +1,45 @@
 import util.Util;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Day1 {
 
     public static void main(String[] args) {
-        var inputs = Util.readInts();
+        var inputs = Util.readStrings();
         Util.submitPart1(part1(inputs));
         Util.submitPart2(part2(inputs));
     }
 
-    private static int part2(List<Integer> inputs) {
-        return 0;
+    private static int part2(List<String> inputs) {
+        List<Integer> w = new ArrayList<>();
+        int total = 0;
+        for (String input : inputs) {
+            if (input.length() == 0) {
+                w.add(total);
+                total = 0;
+            } else {
+                total += Integer.parseInt(input);
+            }
+        }
+        Collections.sort(w);
+        return w.get(w.size()-1) + w.get(w.size()-2) + w.get(w.size()-3);
     }
 
-    private static int part1(List<Integer> inputs) {
-        return 0;
+    private static int part1(List<String> inputs) {
+        int total = 0;
+        int max = 0;
+        for (String input : inputs) {
+            if (input.length() == 0) {
+                if (total > max) {
+                    max = total;
+                }
+                total = 0;
+            } else {
+                total += Integer.parseInt(input);
+            }
+        }
+        return max;
     }
 }
