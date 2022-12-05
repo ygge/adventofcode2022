a4e3e1fb00042cb51814b0f9ba09af4c064e170c
Henrik Ygge <henrik@meepo.se>
2022-12-05 06:22:22 +0100

    cleanup day 5

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -8,32 +8,15 @@ import java.util.List;
 public class Day5 {
 
     public static void main(String[] args) {
+        Util.verifySubmission();
         var inputs = Util.readStrings();
-        //Util.submitPart1(part1(inputs));
+        Util.submitPart1(part1(inputs));
         Util.submitPart2(part2(inputs));
     }
 
     private static String part2(List<String> inputs) {
         List<LinkedList<Character>> stacks = new LinkedList<>();
-        for (int i = 0; i < 9; ++i) {
-            stacks.add(new LinkedList<>());
-        }
-        int r = 0;
-        for (; r < inputs.size() && inputs.get(r).charAt(0) == '['; ++r) {
-            var row = inputs.get(r);
-            int c = 0;
-            for (int i = 1; i < row.length(); i += 4) {
-                char cc = row.charAt(i);
-                if (cc != ' ') {
-                    stacks.get(c).add(cc);
-                }
-                ++c;
-            }
-        }
-        for (LinkedList<Character> characters : stacks) {
-            Collections.reverse(characters);
-        }
-        r += 2;
+        int r = parseStacks(inputs, stacks);
         for (; r < inputs.size(); ++r) {
             var split = inputs.get(r).split(" ");
             int num = Integer.parseInt(split[1]);
@@ -47,6 +30,26 @@ public class Day5 {
             Collections.reverse(list);
             stacks.get(to).addAll(list);
         }
+        return getResult(stacks);
+    }
+
+    private static String part1(List<String> inputs) {
+        List<LinkedList<Character>> stacks = new LinkedList<>();
+        int r = parseStacks(inputs, stacks);
+        for (; r < inputs.size(); ++r) {
+            var split = inputs.get(r).split(" ");
+            int num = Integer.parseInt(split[1]);
+            int from = Integer.parseInt(split[3])-1;
+            int to = Integer.parseInt(split[5])-1;
+            for (int i = 0; i < num; ++i) {
+                char c = stacks.get(from).pollLast();
+                stacks.get(to).add(c);
+            }
+        }
+        return getResult(stacks);
+    }
+
+    private static String getResult(List<LinkedList<Character>> stacks) {
         var sb = new StringBuilder();
         for (LinkedList<Character> stack : stacks) {
             sb.append(stack.peekLast());
@@ -54,16 +57,15 @@ public class Day5 {
         return sb.toString();
     }
 
-    private static String part1(List<String> inputs) {
-        List<LinkedList<Character>> stacks = new LinkedList<>();
-        for (int i = 0; i < 9; ++i) {
-            stacks.add(new LinkedList<>());
-        }
+    private static int parseStacks(List<String> inputs, List<LinkedList<Character>> stacks) {
         int r = 0;
         for (; r < inputs.size() && inputs.get(r).charAt(0) == '['; ++r) {
             var row = inputs.get(r);
             int c = 0;
             for (int i = 1; i < row.length(); i += 4) {
+                if (c == stacks.size()) {
+                    stacks.add(new LinkedList<>());
+                }
                 char cc = row.charAt(i);
                 if (cc != ' ') {
                     stacks.get(c).add(cc);
@@ -75,20 +77,6 @@ public class Day5 {
             Collections.reverse(characters);
         }
         r += 2;
-        for (; r < inputs.size(); ++r) {
-            var split = inputs.get(r).split(" ");
-            int num = Integer.parseInt(split[1]);
-            int from = Integer.parseInt(split[3])-1;
-            int to = Integer.parseInt(split[5])-1;
-            for (int i = 0; i < num; ++i) {
-                char c = stacks.get(from).pollLast();
-                stacks.get(to).add(c);
-            }
-        }
-        var sb = new StringBuilder();
-        for (LinkedList<Character> stack : stacks) {
-            sb.append(stack.peekLast());
-        }
-        return sb.toString();
+        return r;
     }
 }
